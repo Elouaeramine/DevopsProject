866da493d5c2a549cd9d11e17a5facff62cf5c6c
Sarah Benabdallah <sarahbenabdallah@insat.u-carthage.tn>
2022-12-08 16:51:38 +0100

    set jenkins

--- script.groovy
+++ script.groovy
@@ -2,9 +2,9 @@
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t yassine15/demo-app:spring-app-1.0 .'
+        sh 'docker build -t sarahbenabdallah/devopspipeline:spring-app-1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push yassine15/demo-app:spring-app-1.0'
+        sh 'docker push sarahbenabdallah/devopspipeline:spring-app-1.0'
     }
 }
 
@@ -26,19 +26,5 @@ def sonarScan() {
         
 }
 
-def deployApp(String serverIp, String serverUser) {
-    echo 'deploying the application...'
-    def composeRun = '"export MYSQLDB_USER=root MYSQLDB_ROOT_PASSWORD=sofiene MYSQLDB_DATABASE=pet_store MYSQLDB_LOCAL_PORT=3306 MYSQLDB_DOCKER_PORT=3306 SPRING_LOCAL_PORT=8080 SPRING_DOCKER_PORT=8080 && docker-compose up -d"'
-    sshagent (credentials: ['deployment-server']) {
-        sh "ssh -o StrictHostKeyChecking=no ${serverUser}@${serverIp} ${composeRun}"
-    }
-}
-
-def cleanUntaggedImages(String serverIp, String serverUser){
-    def cleanImages = 'docker image prune --force --filter "dangling=true"'
-    sshagent (credentials: ['jenkins-server']) {
-        sh "ssh -o StrictHostKeyChecking=no ${serverUser}@${serverIp} ${cleanImages}"
-    }
-}
 
 return this

a9d62e0074b5195d880761aff6f6a12aa03e1c9a
ElouaerAmine <elouaeramine266@hotmail.com>
2022-12-08 16:46:27 +0100

    fix

--- script.groovy
+++ script.groovy
@@ -26,19 +26,4 @@ def sonarScan() {
         
 }
 
-def deployApp(String serverIp, String serverUser) {
-    echo 'deploying the application...'
-    def composeRun = '"export MYSQLDB_USER=root MYSQLDB_ROOT_PASSWORD=sofiene MYSQLDB_DATABASE=pet_store MYSQLDB_LOCAL_PORT=3306 MYSQLDB_DOCKER_PORT=3306 SPRING_LOCAL_PORT=8080 SPRING_DOCKER_PORT=8080 && docker-compose up -d"'
-    sshagent (credentials: ['deployment-server']) {
-        sh "ssh -o StrictHostKeyChecking=no ${serverUser}@${serverIp} ${composeRun}"
-    }
-}
-
-def cleanUntaggedImages(String serverIp, String serverUser){
-    def cleanImages = 'docker image prune --force --filter "dangling=true"'
-    sshagent (credentials: ['jenkins-server']) {
-        sh "ssh -o StrictHostKeyChecking=no ${serverUser}@${serverIp} ${cleanImages}"
-    }
-}
-
 return this

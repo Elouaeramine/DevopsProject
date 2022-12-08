74c3ff1dd77f0e5cee2e84374f0b9666bec87c07
ElouaerAmine <elouaeramine266@hotmail.com>
2022-12-08 16:11:30 +0100

    add : vendor Tests

---
+++ src/test/java/tp/devops/VendorRepositoryTests.java
@@ -0,0 +1,69 @@
+package tp.devops;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import tp.devops.entities.Customer;
+import tp.devops.entities.Vendor;
+import tp.devops.repositories.CustomerRepository;
+import tp.devops.repositories.VendorRepository;
+
+import java.util.List;
+
+
+
+@DataJpaTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class VendorRepositoryTests {
+
+    @Autowired
+    private VendorRepository vendorRepository;
+
+    @Test
+    @Order(1)
+    @Rollback(value = false)
+    public void saveTest(){
+        Vendor vendor = new Vendor();
+        vendorRepository.save(vendor);
+        Assertions.assertThat(vendor.getId()).isGreaterThan(0);
+    }
+    @Test
+    @Order(2)
+    public void getVendorTest(){
+
+        Vendor vendor = vendorRepository.findById(1L).get();
+
+        Assertions.assertThat(vendor.getId()).isEqualTo(1L);
+
+    }
+
+    @Test
+    @Order(3)
+    public void getListOfVendorsTest(){
+
+        List<Vendor> vendors = vendorRepository.findAll();
+
+        Assertions.assertThat(vendors.size()).isGreaterThan(0);
+
+    }
+
+    @Test
+    @Order(4)
+    @Rollback(value = false)
+    public void updateVendorTest(){
+
+        Vendor vendor = vendorRepository.findById(1L).get();
+
+        vendor.setName("Amine");
+
+        Vendor vendorUpdated =  vendorRepository.save(vendor);
+
+        Assertions.assertThat(vendorUpdated.getName()).isEqualTo("Amine");
+
+    }
+}
